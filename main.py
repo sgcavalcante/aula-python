5ff8f5407bb9638895db6475195fddc37b42a868
sgc <sgcdani@gmail.comc>
2023-07-24 20:57:19 -0300

    adicionado comentários sobre variavéis

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #Arquivo .py
 #comando print - serve para impressão de dados na tela
 print("Bem vindo ao mundo de python!")
+
+#identação
+
+#variavéis numéricas
+#varivaeis string
+
+
+#casting
+#conversão de vriavéis
+#inteiro para string
+#flot para inteiro
+#etc
